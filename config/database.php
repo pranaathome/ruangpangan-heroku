339afe9f57b73b7c97a3dcef55b6de9acec5b34c
pranaathome <pranayadnaap96@gmail.com>
2018-06-26 22:47:56 +0700

    update database

--- config/database.php
+++ config/database.php
@@ -1,10 +1,10 @@
 <?php
 
-$url = parse_url(getenv("DATABASE_URL"));
-$host = $url["host"]??null;
-$username = $url["user"]??null;
-$password = $url["pass"]??null;
-$database = substr($url["path"], 1)??null;
+// $url = parse_url(getenv("DATABASE_URL"));
+// $host = $url["host"]??null;
+// $username = $url["user"]??null;
+// $password = $url["pass"]??null;
+// $database = substr($url["path"], 1)??null;
 
 return [
 
@@ -19,7 +19,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql_production'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
